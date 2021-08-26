3e21acb6a699f8c6d960e592b363f0627a33d46b
Derrick Hudson <hudsonds@appstate.edu>
2021-08-26 17:23:40 -0400

    1-4 working, stuck on 5

--- HUDSONDERRICKhw1.hs
+++ HUDSONDERRICKhw1.hs
@@ -4,13 +4,43 @@ minlist xs = error "No Solution"
 minList :: [Integer] -> Integer
 minList [] = 0
 minList [x] = x
-minList x:xs = min x (minList xs)
+minList (x:y:xs) |x > y = minList (y:xs)
+                 |x < y = minList (x:xs)
+                 |x == y = minList (x:xs)
 
-min :: Integer -> Integer -> Integer
-min a b
-    | a > b  = b
-    | a < b  = a
+-- multiply together all the elements of a list
+multiplyList :: [Integer] -> Integer
+multiplyList [] = 0
+multiplyList [x] = x
+multiplyList (x:xs) = x * multiplyList xs
 
+-- return True if there is an odd element in a list
+existsOdd :: [Integer] -> Bool
+existsOdd [] = False
+existsOdd [x] = if even x then False else True
+existsOdd (x:xs) | odd x = True
+                 | even x = existsOdd xs
+
+-- return Just x if there is some x in the input that is odd
+findOdd :: [Integer] -> Maybe Integer
+findOdd [] = Nothing
+findOdd [x] = if even x then Nothing else Just x
+findOdd (x:xs)  | odd x = Just x
+                | even x = findOdd xs
+
+-- removes all empty strings from a list
+removeEmpty :: [String] -> [String]
+removeEmpty [] = []
+removeEmpty [""] = []
+-- removeEmpty [x] = [x]
+removeEmpty (x:xs)  | x == "" = removeEmpty xs
+                    | x /= "" = x 
+
+
+-- custom implementation of the parity check
+myEven :: Integer -> Bool
+myEven n = (mod n 2 == 0)
+-- myEven n = if mod n 2 == 0 then True else False
 
 -- using let
 fact2' :: Int -> Int
